3fae0e696ffda0b49e1704c16e03d5a649257edf
CookingFansFans <85647499+KanchanGhotkar@users.noreply.github.com>
2021-06-09 21:23:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Write your code below this line 👇
+print('welcom to bandname generator program')
+city = input('what is the name of your city?: ')
+print('city')
+pet_name = input('what is the name of your pet?: ')
+print('pet_name')
+print("your band name could be " +  city + " " + pet_name )
+
+
