32c1aa403c8f15482ef7dbb21c5401c20263ee33
Coding Fans Fans <85647499+KanchanGhotkar@users.noreply.github.com>
2021-06-10 19:24:07 +0000

    Added new class Number Count

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # Write your code below this line 👇
-print('welcom to bandname generator program')
-city = input('what is the name of your city?: ')
-print('city')
-pet_name = input('what is the name of your pet?: ')
-print('pet_name')
-print("your band name could be " +  city + " " + pet_name )
+#print('welcom to bandname generator program')
+#city = input('what is the name of your city?: ')
+#print('city')
+#pet_name = input('what is the name of your pet?: ')
+#print('pet_name')
+#print("your band name could be " +  city + " " + pet_name )
 
+print('hello'[4])
 
