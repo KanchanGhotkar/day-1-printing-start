ce73351a715f232e2aeb00feccce651af14b52ef
KanchanGhotkar <85647499+KanchanGhotkar@users.noreply.github.com>
2021-06-10 20:27:39 +0100

    Update main.py
    uncommented print city statement

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Write your code below this line 👇
 print('welcom to bandname generator program')
 city = input('what is the name of your city?: ')
-#print('city')
+print('city')
 #pet_name = input('what is the name of your pet?: ')
 #print('pet_name')
 #print("your band name could be " +  city + " " + pet_name )
