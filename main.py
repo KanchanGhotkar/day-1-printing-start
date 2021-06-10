889c033e71a60748e2ccee588229f3e48a1943ed
Coding Fans Fans <85647499+KanchanGhotkar@users.noreply.github.com>
2021-06-10 19:26:11 +0000

    updated band generator prog

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Write your code below this line 👇
-#print('welcom to bandname generator program')
-#city = input('what is the name of your city?: ')
+print('welcom to bandname generator program')
+city = input('what is the name of your city?: ')
 #print('city')
 #pet_name = input('what is the name of your pet?: ')
 #print('pet_name')
